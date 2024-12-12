8fc9c92a025a7fc0c77ebb0488d1c0429806acc1
sniegockajulia <julia.sniegocka@usp.ceu.es>
2024-12-12 04:54:44 +0100

    Update main.js

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ document.addEventListener("DOMContentLoaded", () => {
 );
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/PWA-game/sw.js')
+  navigator.serviceWorker.register('/PWA-Game/sw.js')
     .then(registration => {
       console.log('Service Worker registered successfully:', registration);
     })
