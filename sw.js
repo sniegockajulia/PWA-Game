207f90f6031cd7760df3b43634ba5decb945ba42
sniegockajulia <julia.sniegocka@usp.ceu.es>
2024-12-12 02:11:18 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'shooter-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/Game.js',
+  '/Entity.js',
+  '/Character.js',
+  '/Player.js',
+  '/Opponent.js',
+  '/Shot.js',
+  '/main.js',
+  '/game.css',
+  '/assets/boss.png',
+  '/assets/boss_dead.png',
+  '/assets/game_over.png',
+  '/assets/opponent.png',
+  '/assets/opponent_dead.png',
+  '/assets/player.png',
+  '/assets/player_dead.png',
+  '/assets/shot1.png',
+  '/assets/shot2.png',
+  '/assets/you_win.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
