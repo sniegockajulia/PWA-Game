fe3bf6277c7039efbad23b54b13b7fff3513f5a7
sniegockajulia <julia.sniegocka@usp.ceu.es>
2024-12-12 02:15:26 +0100

    Update main.js

--- main.js
+++ main.js
@@ -46,3 +46,24 @@ if ('serviceWorker' in navigator) {
       console.error('Error registering Service Worker:', error);
     });
 }
+
+let deferredPrompt;
+const installButton = document.getElementById('installButton');
+ 
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  installButton.style.display = 'block';
+ 
+  installButton.addEventListener('click', () => {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted installation');
+      } else {
+        console.log('User declined installation');
+      }
+      deferredPrompt = null;
+    });
+  });
+});
